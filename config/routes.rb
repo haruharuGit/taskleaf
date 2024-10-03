fd1693064fb6641d0fcbffe5b779b80b12118457
haruharuGit <chiharu.fu.54@gmail.com>
2024-10-03 10:34:41 +0900

    RSpecインストールとCapybara準備、FactoryBotインストール
    rails5-5-3まで実施

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
-  get '/login', to:'sessions#new'
-  post '/login', to: 'session#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  resources :tasks
+
+  namespace :admin do
+    resources :users
+  end
+    # get 'users/new'
+    # get 'users/edit'
+    # get 'users/show'
+    # get 'users/index'
+
   root to: 'tasks#index'
+  resources :tasks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_09_10_124218) do
+ActiveRecord::Schema.define(version: 2024_10_01_062249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2024_09_10_124218) do
     t.string "password_digest", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
